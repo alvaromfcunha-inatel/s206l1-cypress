10f70b0ad852c42e28aa79024f36b4741d73dc97
Alvaro <alvaro99.cunha@gmail.com>
2022-10-26 23:45:41 -0300

    addded registerUser function test case

--- cypress/e2e/globalsqaLogin.cy.js
+++ cypress/e2e/globalsqaLogin.cy.js
@@ -7,6 +7,29 @@ const LASTNAME = 'cunha'
 const USERNAME = 'alvarocunha'
 const PASSWORD = 'senha123'
 
+const registerUser = () => {
+  const epoch = Date.now()
+  const firstName = `${epoch}_fname`
+  const lastName = `${epoch}_lname`
+  const username = `${epoch}_username`
+  const password = `${epoch}_password`
+
+  cy.visit(LOGIN_URL)
+
+  cy.get('.btn-link').click()
+
+  cy.get('#firstName').type(firstName)
+  cy.get('#Text1').type(lastName)
+  cy.get('#username').type(username)
+  cy.get('#password').type(password)
+
+  cy.get('.btn-primary').click()
+
+  cy.get('.ng-binding').should('contain.text', 'Registration successful')
+
+  return { username, password }
+}
+
 describe('login test suite', () => {
   it('successful register', () => {
     cy.visit(LOGIN_URL)
@@ -54,6 +77,17 @@ describe('login test suite', () => {
 
     cy.get('#username').type(USERNAME)
     cy.get('#password').type(PASSWORD)
+
+    cy.get('.btn-primary').click()
+
+    cy.get('div.ng-scope > :nth-child(2)').should('contain.text', "You're logged in!!")
+  })
+
+  it('successful login: using registerUser function', () => {
+    const { username, password } = registerUser()
+
+    cy.get('#username').type(username)
+    cy.get('#password').type(password)
 
     cy.get('.btn-primary').click()
 
